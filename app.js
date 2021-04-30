48a855dd037df1f2c963fb49dbfead987c7dfac4
Franco Casas <casassfranco@gmail.com>
2021-04-30 11:27:53 -0300

    some environment variables were created and some dependencies were installed. Working in app.js to start the server

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
 import express from "express";
 import mysql from "mysql";
 import cors from "cors";
-
-// const bodyParser = require("body-parser");
+import morgan from "morgan";
 
 const PORT = process.env.PORT || 4000;
-
 const app = express();
 
-app.use(cors());
+const connection = mysql.createConnection({
+  host:  process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+});
 
+app.use(cors());
+app.options("*", cors());
+
+//Middlewares
 app.use(express.json());
+app.use(morgan("tiny"));
+
+//Routes
+
+//database
+connection.connect((error) => {
+  if (error) throw error;
+  else console.log("Database server running!");
+});
+
+//server
+app.listen(PORT, () => console.log(`Server runing on port ${PORT}`));
